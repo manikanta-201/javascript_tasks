af88292bf9e4028a26fa179a9765ee5aa9999e87
manikanta <manikant66677@gmail.com>
2024-07-30 11:34:07 +0530

    first commit

---
+++ task1/app.js
@@ -0,0 +1,21 @@
+alert("hello world");
+// To exchange the values of two numbers without using Third variable and after exchange then Double of the values of two numbers.
+
+
+// answer
+
+let a=10;
+let b=20;
+// Swap the values without using a third variable
+a = a+b;  // 10+20=30
+b = a-b;  // 30-20=10
+a = a-b;  // 30-10=20
+
+// Double the values
+a = a*2;
+b = b*2;
+
+
+
+console.log(a);
+console.log(b);
